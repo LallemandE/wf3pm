a2dac7ece97f55b14bcc331a907ab8b71513d7fb
LallemandE <job@lallemand.lu>
2018-04-09 21:58:12 +0200

    use of User.php from Romain to solve the problem with lost roles

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -1,38 +1,26 @@
 <?php
-
 namespace App\Entity;
-
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
-// POUR POUVOIR FAIRE DES CONTROLE D'UNICITE, J'AI BESOIN DES 3 USE SUIVANT
-
 use Doctrine\ORM\Mapping as ORM;
-
-// Pour pouvoir indiquer les contrôles à effectuer dans les commentaires devant les attributs
 use Symfony\Component\Validator\Constraints as Assert;
-// pour taper la ligne précédent, on peut taper use NOTNULL puis autocomplétion et retirer la fin.
-
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Ramsey\Uuid\Uuid;
 use Symfony\Component\Security\Core\User\UserInterface;
-use Symfony\Component\Security\Core\User\EquatableInterface;
-
-
-
-
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
  * @UniqueEntity(
- *    fields={"username"},
- *    errorPath="username",
- *    message="This username is already in use !"
- *    )
+ *  fields={"username"},
+ *  errorPath="username",
+ *  message="This username is already in use!"
+ * )
  * @UniqueEntity(
- *    fields={"email"},
- *    errorPath="email",
- *    message="This email is already in use !"
- *    )   
+ *  fields={"email"},
+ *  errorPath="email",
+ *  message="This email is already in use!"
+ * )
  */
-class User implements UserInterface, EquatableInterface
+class User implements UserInterface
 {
     /**
      * @ORM\Id()
@@ -40,213 +28,170 @@ class User implements UserInterface, EquatableInterface
      * @ORM\Column(type="integer")
      */
     private $id;
-
-    /**
-     * @ORM\Column(type="string", length=255)
-     * @Assert\Length(
-     *      min = 4,
-     *      max = 50,
-     *      minMessage = "Your first name must be at least {{ limit }} characters long",
-     *      maxMessage = "Your first name cannot be longer than {{ limit }} characters"
-     * )
-     */
-    private $username;
-
     /**
      * @ORM\Column(type="string", length=255)
      * @Assert\NotBlank()
-     * 
+     */
+    private $username;
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank()
      */
     private $firstname;
-
     /**
      * @ORM\Column(type="string", length=255)
      * @Assert\NotBlank()
      */
     private $lastname;
-
-    
     /**
      * @ORM\Column(type="string", length=255)
-     * @Assert\Email(
-     *     message = "The email '{{ value }}' is not a valid email.",
-     *     checkMX = true
-     * )
+     * @Assert\Email()
      */
     private $email;
-    
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Regex(
+     *  pattern="/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-,]).{8,}$/",
+     *  message="The password must follow: minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character"
+     *  )
      */
     private $password;
-    
     /**
      * @ORM\Column(type="boolean")
      */
     private $active = false;
-
     /**
      * @ORM\Column(type="string", length=255, nullable=true)
      */
     private $emailToken;
-
     /**
      * @ORM\ManyToMany(targetEntity="App\Entity\Role")
      */
     private $roles = [];
-
     /**
      * @ORM\Column(type="string", length=255)
      */
     private $salt;
-    
     public function __construct()
     {
         $this->setEmailToken(Uuid::uuid1());
+        $this->roles = new ArrayCollection();
     }
-
+    
     public function getId()
     {
         return $this->id;
     }
-
     public function getUsername(): ?string
     {
         return $this->username;
     }
-
     public function setUsername(string $username): self
     {
         $this->username = $username;
-
         return $this;
     }
-
     public function getFirstname(): ?string
     {
         return $this->firstname;
     }
-
     public function setFirstname(string $firstname): self
     {
         $this->firstname = $firstname;
-
         return $this;
     }
-
     public function getLastname(): ?string
     {
         return $this->lastname;
     }
-
     public function setLastname(string $lastname): self
     {
         $this->lastname = $lastname;
-
         return $this;
     }
-
-    
     public function getEmail(): ?string
     {
         return $this->email;
     }
-    
     public function setEmail(string $email): self
     {
         $this->email = $email;
-        
         return $this;
     }
-    
-    
     
     public function getPassword(): ?string
     {
         return $this->password;
     }
-
     public function setPassword(string $password): self
     {
         $this->password = $password;
-
         return $this;
     }
-    
-    public function getActive(): bool
+    public function getActive(): ?bool
     {
         return $this->active;
     }
-    
     public function setActive(bool $active): self
     {
         $this->active = $active;
-        
         return $this;
     }
-
     public function getEmailToken(): ?string
     {
         return $this->emailToken;
     }
-
     public function setEmailToken(?string $emailToken): self
     {
         $this->emailToken = $emailToken;
-
         return $this;
     }
-
-    /**
-     * @return array[]
-     */
-    public function getRoles(): array
-    {
-        $strings = [];
-        foreach($this->roles as $role) {
-            $string[] = $role->getLabel();
-        }
-            
-        return $strings;
-    }
-
+    
     public function addRole(Role $role): self
     {
         if (!$this->roles->contains($role)) {
             $this->roles[] = $role;
         }
-
         return $this;
     }
-
     public function removeRole(Role $role): self
     {
         if ($this->roles->contains($role)) {
             $this->roles->removeElement($role);
         }
-
         return $this;
     }
-
     public function getSalt(): ?string
     {
         return $this->salt;
     }
-
     public function setSalt(string $salt): self
     {
         $this->salt = $salt;
-
         return $this;
     }
     
-    public function eraseCredentials(){
-        return;
+    /**
+     * @return array
+     */
+    public function getRoles(): array
+    {
+        $strings = [];
+        foreach ($this->roles as $role) {
+            $strings[] = $role->getLabel();
+        }
+        
+        return $strings;
     }
     
-    public function isEqualTo(UserInterface $user)
+    /**
+     * Removes sensitive data from the user.
+     * 
+     * This is important if, at any given point, sensitive information like
+     * the plain-text password is stored on this object.
+     */
+    public function eraseCredentials()
     {
-        return $user->getUsername() == $this->getUsername();
+        return;
     }
-
 }
