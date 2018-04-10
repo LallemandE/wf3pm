caffc057acb8d878f5b266bfcad798580c512ee4
LallemandE <job@lallemand.lu>
2018-04-10 22:39:48 +0200

    js commentFileAdder.js

--- public/js/commentFileAdder.js
+++ public/js/commentFileAdder.js
@@ -3,21 +3,30 @@ Ce programme doit être adapté pour tenir compte du fichier html spécifique
 
 */
 function addFileInput(){
+	
+	// je cherche l'élément correspondant à #comment_files
+	
 	var prototype = $('#comment_files').data('prototype');
 	var count = $('#comment_files > .form-group').length;
 	
 
+	// je crée une nouvelle variable et je remplace le pattern __name__ par le nombre d'élément
+	// que j'ai déjà compté ci-dessus
 	var newForm = prototype.replace(/__name__/g, count);
+	
 	
 	var group = $('.form-group', newForm);
 	
+	// j'ajoute le group que je viens de construire au bloc
 	$('#comment_files').append(group);
 }
 
 var button = $('<button>Add file</button>');
-button.addClass('btn btn-success');
+
+// Je n'utilise pas bootstrap => je n'ai pas besoin de ce code de Matthieu
+// button.addClass('btn btn-success');
 button.attr('type', 'button');
 button.on('click', addFileInput);
 
-
+// je met derrière le bouton que je viens de créer.
 $("#comment_files").after(button);
